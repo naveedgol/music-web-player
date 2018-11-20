7dcc25f07b180653acff60a34dd1d4e7c4dbec22
Naveed Golafshani <ngolafshani@live.ca>
2018-11-19 22:06:32 -0800

    Fix build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ng build --prod --aot --output-path docs --base-href /apple-music-web-player/
-sed -i '' 's/const req = event.request;/const req = event.request;if(req.url.toLowerCase().includes('apple')){return;}/' docs/ngsw-worker.js
+sed -i '' "s/const req = event.request;/const req = event.request;if(req.url.toLowerCase().includes('apple')){return;}/" docs/ngsw-worker.js
 cp docs/index.html docs/404.html
