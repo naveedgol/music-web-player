772e5ef0a29149684d4b6101636a49c744bd2ef3
Naveed Golafshani <ngolafshani@live.ca>
2018-11-24 10:10:25 -0800

    Fix Build script pathing

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ng build --prod --aot --base-href /apple-music-web-player/
-sed -i '' "s/const req = event.request;/const req = event.request;if(req.url.toLowerCase().includes('apple')){return;}/" docs/ngsw-worker.js
+sed -i '' "s/const req = event.request;/const req = event.request;if(req.url.toLowerCase().includes('apple')){return;}/" dist/apple-music-web-player/ngsw-worker.js
