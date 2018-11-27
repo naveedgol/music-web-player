a6b5ea02f3bf359701449115e1c4f4689e9d6594
Naveed Golafshani <ngolafshani@live.ca>
2018-11-26 21:28:40 -0800

    Support lazy loading on all browsers

--- src/app/directives/lazy-load.directive.ts
+++ src/app/directives/lazy-load.directive.ts
@@ -10,18 +10,26 @@ export class LazyLoadDirective implements AfterViewInit {
   constructor(private el: ElementRef) {}
 
   ngAfterViewInit() {
-    this.lazyLoadImage();
+    this.canLazyLoad() ? this.lazyLoadImage() : this.loadImage();
+  }
+
+  private canLazyLoad() {
+    return window && 'IntersectionObserver' in window;
   }
 
   private lazyLoadImage() {
     const obs = new IntersectionObserver(entries => {
       entries.forEach(({ isIntersecting }) => {
         if (isIntersecting) {
-          this.srcAttr = this.src;
+          this.loadImage();
           obs.unobserve(this.el.nativeElement);
         }
       });
     });
     obs.observe(this.el.nativeElement);
+  }
+
+  private loadImage() {
+    this.srcAttr = this.src;
   }
 }
