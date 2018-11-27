09da6cbb12ea2e33460c69abc88751035cb9a013
Naveed Golafshani <ngolafshani@live.ca>
2018-11-26 17:18:38 -0800

    Update build process

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 ng build --prod --aot --base-href /apple-music-web-player/
 sed -i '' "s/const req = event.request;/const req = event.request;if(req.url.toLowerCase().includes('apple')){return;}/" dist/apple-music-web-player/ngsw-worker.js
+git checkout gh-pages
+cp -r dist/apple-music-web-player/* .
