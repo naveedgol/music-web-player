a168b8af176f53eb89ff4f66be4b2d644964b5d2
Naveed Golafshani <ngolafshani@live.ca>
2018-11-29 19:59:56 -0800

    Update manifest settings, remove build script

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-ng build --prod --aot --base-href /apple-music-web-player/
-sed -i '' "s/const req = event.request;/const req = event.request;if(req.url.toLowerCase().includes('apple')){return;}/" dist/apple-music-web-player/ngsw-worker.js
-git checkout gh-pages
-cp -r dist/apple-music-web-player/* .
-git add -A
-git commit -m "Rebuild"
-git push
-git checkout master
