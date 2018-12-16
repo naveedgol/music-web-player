53733fa89984f3feb7675d997ace96ed5404efcb
Naveed Golafshani <ngolafshani@live.ca>
2018-12-15 18:17:15 -0800

    #24 Hide apple music token into build process

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   // tslint:disable-next-line:max-line-length
-  token: 'eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkY5UE1TRjNONVUifQ.eyJpc3MiOiJBVVdWNExLQ0tMIiwiaWF0IjoxNTQ0MTU2NzEyLCJleHAiOjE1NTk5MzAxMTJ9.EH79zyt8vLPQvIll9s6bZz2No6dD_cfY0tx2ibYLNN82GcWN4WAmWkFuSNIWC62AlhqEXRx1eeTCkxSSat9TDQ'
+  token: 'APPLE_MUSIC_TOKEN'
 };
 
 /*
