52b7e98b160ca5e05a0bac8b245945c5195f7f44
Vertana <Vertana@protonmail.ch>
2018-12-15 21:17:25 -0800

    Adds Media Key Support
    Allows users to use media keys on the site.

--- src/app/components/track-controls/track-controls.component.ts
+++ src/app/components/track-controls/track-controls.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild, HostListener, Input, EventEmitter, Output } from 
 import { PlayerService, PlaybackStates } from '../../services/player.service';
 import { MatBottomSheet } from '@angular/material';
 import { SettingsComponent } from '../settings/settings.component';
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
 
 @Component({
   selector: 'app-track-controls',
@@ -21,25 +22,70 @@ export class TrackControlsComponent {
   constructor(
     public playerService: PlayerService,
     private bottomSheet: MatBottomSheet
-    ) {
+  ) {
   }
 
-  @HostListener('document:keydown.space', ['$event']) onSpaceKeydownHandler(event: KeyboardEvent) {
-    if ( event.srcElement.tagName !== 'INPUT' ) {
+  @HostListener('document:keydown', ['$event']) KeydownHandler(event: KeyboardEvent) {
+    if (event.srcElement.tagName !== 'INPUT') {
       event.preventDefault();
-      this.togglePlayPause();
+      switch (event.key) {
+        case ' ':
+          this.togglePlayPause();
+          break;
+          case 'space':
+          this.togglePlayPause();
+          break;
+        case 'p':
+          this.togglePlayPause();
+          break;
+        case 'MediaPlay':
+          this.togglePlayPause();
+          break;
+        case 'MediaTrackNext':
+          this.skipToNextItem();
+          break;
+        case 'ArrowRight':
+          this.skipToNextItem();
+          break;
+        case 'MediaTrackPrevious':
+          this.skipToPreviousItem();
+          break;
+        case 'ArrowLeft':
+          this.skipToPreviousItem();
+          break;
+        case 'ArrowUp':
+          if (this.lastVolume + .2 >= 10 || this.lastVolume === 10) {
+            this.changeVolume(10);
+            this.volumeSlider.value = 10;
+            break;
+          }
+          this.changeVolume(this.lastVolume + .2);
+          this.volumeSlider.value = this.lastVolume + .2;
+          break;
+        case 'ArrowDown':
+          if (this.lastVolume - .2 <= 0 || this.lastVolume === 0) {
+            this.changeVolume(0);
+            this.volumeSlider.value = 0;
+            break;
+          }
+          this.changeVolume(this.lastVolume - .2);
+          this.volumeSlider.value = this.lastVolume - .2;
+          break;
+        default:
+          break;
+      }
     }
   }
 
   get isLoading(): boolean {
     return this.playerService.playbackState === this.playbackStates.LOADING ||
-           this.playerService.playbackState === this.playbackStates.ENDED ||
-           this.playerService.playbackState === this.playbackStates.WAITING ||
-           this.playerService.playbackState === this.playbackStates.STALLED;
+      this.playerService.playbackState === this.playbackStates.ENDED ||
+      this.playerService.playbackState === this.playbackStates.WAITING ||
+      this.playerService.playbackState === this.playbackStates.STALLED;
   }
 
   togglePlayPause(): void {
-    if ( this.playerService.playbackState === this.playbackStates.PAUSED ) {
+    if (this.playerService.playbackState === this.playbackStates.PAUSED) {
       this.playerService.play().subscribe();
     } else {
       this.playerService.pause().subscribe();
@@ -54,21 +100,21 @@ export class TrackControlsComponent {
     this.playerService.skipToPreviousItem().subscribe();
   }
 
-  seekToTime( time: number ): void {
-    this.playerService.seekToTime( time ).subscribe();
+  seekToTime(time: number): void {
+    this.playerService.seekToTime(time).subscribe();
   }
 
-  changeVolume( volume: number ): void {
+  changeVolume(volume: number): void {
     this.lastVolume = volume;
-    this.playerService.changeVolume( volume / 10 );
+    this.playerService.changeVolume(volume / 10);
   }
 
-  toggleMute( volume: number ): void {
-    if ( this.volumeSlider.value > 0 ) {
-      this.playerService.changeVolume( 0 );
+  toggleMute(volume: number): void {
+    if (this.volumeSlider.value > 0) {
+      this.playerService.changeVolume(0);
       this.volumeSlider.value = 0;
     } else {
-      this.playerService.changeVolume( this.lastVolume / 10 );
+      this.playerService.changeVolume(this.lastVolume / 10);
       this.volumeSlider.value = this.lastVolume;
     }
   }
