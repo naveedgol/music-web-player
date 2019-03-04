e8208e6dadd48bc2854f507f0b3cd699481cce78
Fonger <5862369+Fonger@users.noreply.github.com>
2019-03-04 11:37:23 +0800

    fix project name

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ng build --prod --aot
-sed -i "/onFetch(event) {/,/const req = event.request;/c onFetch(event){const req=event.request;const whitelist=['apple'];if(whitelist.some(word=>req.url.toLowerCase().includes(word.toLowerCase()))){return}" ./dist/apple-music-web-player/ngsw-worker.js
-sed -i "s/APPLE_MUSIC_TOKEN/$1/" ./dist/apple-music-web-player/main.*.js
+sed -i "/onFetch(event) {/,/const req = event.request;/c onFetch(event){const req=event.request;const whitelist=['apple'];if(whitelist.some(word=>req.url.toLowerCase().includes(word.toLowerCase()))){return}" ./dist/music-web-player/ngsw-worker.js
+sed -i "s/APPLE_MUSIC_TOKEN/$1/" ./dist/music-web-player/main.*.js
